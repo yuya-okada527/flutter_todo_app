d58216ded35d585ec14f13e4fd74d2ec7f9f812a
yuya-okada527 <yuya.okada527@gmail.com>
2022-04-10 15:28:34 +0900

    add confirm modal

--- lib/main.dart
+++ lib/main.dart
@@ -35,37 +35,63 @@ class _TodoListPageState extends State<TodoListPage> {
       appBar: AppBar(
         title: const Text("リスト一覧"),
       ),
-      body: _todoList.isNotEmpty ? ListView.builder(
-        itemCount: _todoList.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              title: Text(_todoList[index]),
-              trailing: IconButton(
-                onPressed: () {
-                  setState(() {
-                    _todoList.removeAt(index);
-                  });
-                },
-                icon: const Icon(
-                  Icons.delete,
-                  color: Colors.redAccent,
+      body: _todoList.isNotEmpty
+          ? ListView.builder(
+              itemCount: _todoList.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(_todoList[index]),
+                    trailing: IconButton(
+                      onPressed: () async {
+                        // アラートの表示
+                        var result = await showDialog<bool>(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('確認'),
+                                content: Text("「${_todoList[index]}」を削除します。"),
+                                actions: <Widget>[
+                                  ElevatedButton(
+                                    child: const Text("キャンセル"),
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(false),
+                                  ),
+                                  ElevatedButton(
+                                    child: const Text("確認"),
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(true),
+                                  ),
+                                ],
+                              );
+                            });
+                        if (result != null && result) {
+                          // タスクリストの更新
+                          setState(() {
+                            _todoList.removeAt(index);
+                          });
+                        }
+                      },
+                      icon: const Icon(
+                        Icons.delete,
+                        color: Colors.redAccent,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            )
+          : const Padding(
+              padding: EdgeInsets.all(16),
+              child: Text(
+                "タスクがありません。",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Colors.black54,
                 ),
               ),
             ),
-          );
-        },
-      ) : const Padding(
-        padding: EdgeInsets.all(16),
-        child: Text(
-          "タスクがありません。",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
-            color: Colors.black54,
-          ),
-        ),
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           final newListText = await Navigator.of(context).push(
