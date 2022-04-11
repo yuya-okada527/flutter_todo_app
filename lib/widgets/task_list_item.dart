74f63f9d1a174cdd887f3f14dc5226a9e1dba70a
yuya-okada527 <yuya.okada527@gmail.com>
2022-04-11 22:44:58 +0900

    format

--- lib/widgets/task_list_item.dart
+++ lib/widgets/task_list_item.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class TaskListItem extends StatelessWidget {
-  const TaskListItem({Key? key, required this.title, required this.onDeleteConfirm}) : super(key: key);
+  const TaskListItem(
+      {Key? key, required this.title, required this.onDeleteConfirm})
+      : super(key: key);
 
   final String title;
   final Function onDeleteConfirm;
@@ -12,40 +14,36 @@ class TaskListItem extends StatelessWidget {
       child: ListTile(
         title: Text(
           title,
-          style: const TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 18
-          ),
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
         ),
         trailing: IconButton(
           onPressed: () async {
             // アラートの表示
             var result = await showDialog<bool>(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: const Text("確認"),
-                  content: Text("「${title}」を削除します。"),
-                  actions: <Widget>[
-                    ElevatedButton(
-                      child: const Text("キャンセル"),
-                      onPressed: () {
-                        return Navigator.of(context).pop(false);
-                      },
-                    ),
-                    ElevatedButton(
-                      child: const Text(
-                        "確認",
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text("確認"),
+                    content: Text("「$title」を削除します。"),
+                    actions: <Widget>[
+                      ElevatedButton(
+                        child: const Text("キャンセル"),
+                        onPressed: () {
+                          return Navigator.of(context).pop(false);
+                        },
                       ),
-                      onPressed: () {
-                        return Navigator.of(context).pop(true);
-                      },
-                    ),
-                  ],
-                );
-              }
-            );
+                      ElevatedButton(
+                        child: const Text(
+                          "確認",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        onPressed: () {
+                          return Navigator.of(context).pop(true);
+                        },
+                      ),
+                    ],
+                  );
+                });
             if (result != null && result) {
               // タスクリストの更新
               onDeleteConfirm();
